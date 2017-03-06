d3d9be1a45b45ff2d06d3f54f09743980468328b
bazzorwowpwn <max.ydstrom@gmail.com>
2017-03-06 17:01:25 +0100

    Update 123456789.js

--- 123456789.js
+++ 123456789.js
@@ -1,7 +1,7 @@
 // BRUNA RUTA NUMMER 1
 			
 // Byta rubrik
-var x = "Kycklinar springer snavbbt";
+var x = "Kycklinar springer snavbbbt";
 // byta underrubrik/text
 var y = "Detta raporter Stina Lindgren från Stenholmens dagblad";
 // byta länk namn
