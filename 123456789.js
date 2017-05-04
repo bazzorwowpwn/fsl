5ca7b7c4d170f1ee5e168bdb1fceedc00f977fd0
bazzorwowpwn <max.ydstrom@gmail.com>
2017-05-04 21:00:04 +0200

    Update 123456789.js

--- 123456789.js
+++ 123456789.js
@@ -18,14 +18,14 @@ document.getElementById("1.3").innerHTML = z;
 // BRUNA RUTA NUMMER 2
 			
 // Byta rubrik
-var a = "Träningstext till förstasidan";
+var a = "Retroaktiv barmhärtighet i stället för utveckling?";
 // byta under rubrik/text
-var b = "Träningstext 2";
+var b = "På branschorganisationen Svensk Vindenergis hemsida ligger en artikel med rubriken<br><br>Låt inte de som gått i spetsen ta kostnaderna.";
 // byta länk namn
-var c = "<i> läs mer </i>";
+var c = "läs mer";
 // byta länk adress, ändra endast webadressen innom citattäcknena,
 var yourElement2 = document.getElementById('2.3');
-yourElement2.setAttribute('href', 'http://www.landskapsskydd.se/artikel/JH103');
+yourElement2.setAttribute('href', 'http://landskapsskydd.se/artikel/JH101');
     
     
 document.getElementById("2.1").innerHTML = a;
